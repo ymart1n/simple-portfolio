f829d8028f3c7c1288cfa3bf9ef484f474b0565e
Zhaohang Yan <10969mart1n@gmail.com>
2022-06-24 18:54:04 -0400

    .

--- frontend/rest-api/index.js
+++ frontend/rest-api/index.js
@@ -8,48 +8,43 @@ const cors = require("cors");
 /// FRONTEND REST-API CONFIG
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-
 /// DB CONNECTION
-const mongoString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@coding-blog.rv1qo.mongodb.net/blog?retryWrites=true&w=majority`;
+const mongoString = process.env.DB_CONNECTION;
 
 mongoose.connect(mongoString, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
 });
 
 mongoose.connection.on("error", (error) => {
-    if (process.env.NODE_ENV === "develoment") {
-        console.log(error);
-    }
+  if (process.env.NODE_ENV === "development") {
+    console.log(error);
+  }
 });
 
 mongoose.connection.on("open", () => {
-    console.log("Connected to MongoDB database");
+  console.log("Connected to MongoDB database");
 });
-
 
 /// REST-API CONFIG
 app.use(cors());
 
-
 /// FRONTEND REST-API CONFIG
 app.use(helmet());
 
-
 /// ROUTES
 app.get("/", (_, res) => {
-    res.send("webstie-api is working, kel!")
+  res.send("webstie-api is working, kel!");
 });
 
 app.use(require("./routes/index.js"));
 
-
 /// RUN SERVER
 app.listen(PORT, () => {
-    console.log(`Express app listening on port ${PORT}`);
+  console.log(`Express app listening on port ${PORT}`);
 });
--- frontend/website/next.config.js
+++ frontend/website/next.config.js
@@ -1,31 +1,31 @@
 const withPWA = require("next-pwa");
 
 module.exports = withPWA({
-    images: {
-        domains: ["i.imgur.com"],
-    },
+  images: {
+    domains: ["i.imgur.com"],
+  },
 
-    env: {
-        DEV_API_URL: "http://localhost:5000",
-        PRODUCTION_API_URL: "https://krehwell-rest-api.vercel.app",
-    },
+  env: {
+    DEV_API_URL: "http://localhost:3001",
+    PRODUCTION_API_URL: "https://zhaohyan22-api.vercel.app",
+  },
 
-    pwa: {
-        disable: process.env.NODE_ENV === "development",
-        dest: "public",
-    },
+  pwa: {
+    disable: process.env.NODE_ENV === "development",
+    dest: "public",
+  },
 
-    webpack: (config) => {
-        /* https://github.com/leerob/leerob.io/blob/main/next.config.js */
+  webpack: (config) => {
+    /* https://github.com/leerob/leerob.io/blob/main/next.config.js */
 
-        // Object.assign(config.resolve.alias, {
-        //   react: 'preact/compat',
-        //   'react-dom/test-utils': 'preact/test-utils',
-        //   'react-dom': 'preact/compat'
-        // });
+    // Object.assign(config.resolve.alias, {
+    //   react: 'preact/compat',
+    //   'react-dom/test-utils': 'preact/test-utils',
+    //   'react-dom': 'preact/compat'
+    // });
 
-        return config;
-    },
+    return config;
+  },
 
-    swcMinify: true,
+  swcMinify: true,
 });
