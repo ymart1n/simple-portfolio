fb810d72b721c2df8c1fb47218fbe995b5ed4794
Zhaohang Yan <10969mart1n@gmail.com>
2022-06-27 11:57:50 -0400

    finishing timeline

--- frontend/website/components/Timeline.js
+++ frontend/website/components/Timeline.js
@@ -27,29 +27,55 @@ const FullTimeline = () => (
     <Year>2018</Year>
     <ul>
       <Step title="Study Abroad 🛫 🌅" succeed>
-        First time being away from home for 12,023 km and four years.
+        <ul>
+          <li>First time being away from home for 12,023 km and four years.</li>
+        </ul>
       </Step>
       <Step title="Graduate High School 🥳 🎉" succeed />
       <Step title="Learn How to Program 👨🏻‍💻" succeed>
-        Along with reading the{" "}
-        <a
-          href="https://ehmatthes.github.io/pcc/"
-          rel="noreferrer"
-          target="_blank"
-        >
-          Python Crash Course
-        </a>
-        , I wrote my first line of code in Python. It may appear to be a bit
-        late in comparison to people who begin coding as children. But this is
-        the beginning of my story.
+        <ul>
+          <li>
+            Along with reading the{" "}
+            <a
+              href="https://ehmatthes.github.io/pcc/"
+              rel="noreferrer"
+              target="_blank"
+            >
+              Python Crash Course
+            </a>
+            , I wrote my first line of code in Python. It may appear to be a bit
+            late in comparison to people who begin coding as children. But this
+            is the beginning of my story.
+          </li>
+        </ul>
+      </Step>
+    </ul>
+    <Divider />
+    <Year>2017</Year>
+    <ul>
+      <Step title="SAT test in Hong Kong ✍🏻" succeed />
+      <Step title="IELTS test 🗣️" succeed />
+    </ul>
+    <Divider />
+    <Year>2015</Year>
+    <ul>
+      <Step title="Graduate Junior High School" succeed>
+        <ul>
+          <li>Got into the second best high school in Fuzhou, Fujian.</li>
+        </ul>
       </Step>
     </ul>
     <Divider />
     <Year>2016</Year>
     <ul>
       <Step title="Lowest Physics Score in School ⚛️ 📉" succeed>
-        Monthly physics test, I decided not to show off myself that I'm a genius
-        man. <br></br>100 marks, I lost 73 marks. TT
+        <ul>
+          <li>
+            {" "}
+            Monthly physics test, I decided not to show off myself that I'm a
+            genius man. <br></br>100 marks, I lost 73 marks. TT
+          </li>
+        </ul>
       </Step>
       <Step title="First Soccer Game with refs ⚽" succeed />
       <Step title="First Basketball Game with refs 🏀" succeed />
@@ -58,30 +84,46 @@ const FullTimeline = () => (
     <Year>2015</Year>
     <ul>
       <Step title="Graduate Junior High School" succeed>
-        Got into the second best high school in Fuzhou, Fujian.
+        <ul>
+          <li>Got into the second best high school in Fuzhou, Fujian.</li>
+        </ul>
       </Step>
     </ul>
     <Divider />
     <Year>2012</Year>
     <ul>
       <Step title="#1 in Class for First In Junior High School" succeed>
-        Never in my life to be in a class with full of genius but still secure
-        #1. I couldn't even believe myself at a time.
+        <ul>
+          <li>
+            {" "}
+            Never in my life to be in a class with full of genius but still
+            secure #1. I couldn't even believe myself at a time.
+          </li>
+        </ul>
       </Step>
       <Step title="#1 in Class for Second Time">
-        Unfortunately to be #1 can only persist for one semester. But I was
-        always top 5 in class for the rest of semesters ;p.
+        <ul>
+          <li>
+            Unfortunately to be #1 can only persist for one semester. But I was
+            always top 5 in class for the rest of semesters in Junior High
+            School ;p.
+          </li>
+        </ul>
       </Step>
     </ul>
     <Divider />
     <Year>2007</Year>
     <ul>
       <Step title="First Computer" succeed>
-        One computer to all members in the family. Windows XP with interesting
-        software really opened my eyes. Most of them are pirated software like
-        Pokemon games, GBA simulator, etc (even the operating system itself was
-        pirated XD). I'm very grateful for the amount of information I absorbed
-        during the technology boom at my early age.
+        <ul>
+          <li>
+            One computer to all members in the family. Windows XP with
+            interesting software really opened my eyes. Most of them are pirated
+            software like Pokemon games, GBA simulator, etc (even the operating
+            system itself was pirated XD). I'm very grateful for the amount of
+            information I absorbed during the technology boom at my early age.
+          </li>
+        </ul>
       </Step>
     </ul>
     <Divider />
@@ -98,118 +140,211 @@ export default function Timeline() {
   return (
     <>
       <h2>📆 Timeline</h2>
-      <p>
+      {/* <p>
         🏗️ 2019 - 2022 Timeline is under construction, coming soon. <br></br>
         👷🏻 Click 'Load More' button to see timeline before 2019.
-      </p>
-      {/* <Year>2021</Year>
+      </p> */}
+      <Year>2022</Year>
       <ul>
-        <Step title="Graduate University 🎓" succeed>
-          Finally get rid of those salty, unskillful, arrogant lecturers 🎉.
+        <Step title="Start new journey at UBC 🏖️" inProgress />
+        <Step title="Graduate from UofT 🎓 🎉" succeed />
+        <Step title="First contribution to open-source 📖" succeed>
+          <ul>
+            <li>
+              Opened my first issue on GitHub and Created a{" "}
+              <a
+                href="https://github.com/mattcone/markdown-guide/pull/98"
+                rel="noreferrer"
+                target="_blank"
+              >
+                pull request{" "}
+              </a>
+              in mattcone/markdown-guide. The PR was merged. It might sounds
+              trivial but it was a milestone for me.
+            </li>
+          </ul>
         </Step>
-        <Step title="Learn Fastify" succeed />
-        <Step title="Get Onboard with Knowt 💼" succeed>
-          A post on Reddit attracted them to get me on board with them :).
+        <Step title="Learn Next.js" inProgress />
+        <Step title="BonZeb CV Project Finished 🐟" succeed />
+        <Step title="Learn Nest.js" inProgress />
+        <Step title="Learn Google Cloud Platform" inProgress />
+        <Step title="mdTogether Made 📝" succeed>
+          <ul>
+            <li>
+              <a
+                href="https://github.com/ymart1n/mdTogether"
+                rel="noreferrer"
+                target="_blank"
+              >
+                mdtogether
+              </a>{" "}
+              is an Online Collaborative Markdown Editor that provides features
+              like Editing & Previewing Markdown documents, collaborating with
+              others Synchronously or Asynchronously on the same Markdown
+              document, having a Video Call during collaboration, picking Five
+              Colorful Themes for the editor, and Exporting Markdown documents
+              to various formats (i.e., .pdf, .md, .html).
+            </li>
+            <li>
+              {" "}
+              It was developed using React.js, Experss.js, GraphQL, MongoDB and
+              deployed using Nginx, Digital Ocean, and Docker
+            </li>
+          </ul>
         </Step>
-        <Step title="Migrate to Arch Linux " succeed>
-          Previously using Manjaro dual boot with Windows. Unfortunately Manjaro
-          doesn't really seem to "just-work". Migrate to Arch, Love it!
-          Arch+i3wm now and it's perfect.
+        <Step title="Grinding LeetCode" inProgress />
+      </ul>
+      <Year>2021</Year>
+      <ul>
+        <Step title="Low-Code Research Paper Published 🔬" succeed>
+          <ul>
+            <li>
+              Published a low-code development{" "}
+              <a
+                href="https://www.researchgate.net/publication/357417399_The_Impacts_of_LowNo-Code_Development_on_Digital_Transformation_and_Software_Development"
+                rel="noreferrer"
+                target="_blank"
+              >
+                research paper
+              </a>
+              , The Impacts of Low/No-Code Development on Digital Transformation
+              and Software Development. For more information of the paper, have
+              a look at it on{" "}
+              <a
+                href="https://arxiv.org/abs/2112.14073"
+                rel="noreferrer"
+                target="_blank"
+              >
+                arXiv
+              </a>
+              .
+            </li>
+            <li>
+              Low/No-code development is a software development method that
+              provides users with a platform for visually creating applications
+              with little or no coding. In the paper, I explored the benefits
+              &amp; limitations of Low/No-Code development and modern
+              Low/No-Code development platforms in the industry.
+            </li>
+          </ul>
         </Step>
-        <Step title="This Blog Made" succeed>
-          This blog is my first fullstack web project with an additional custom
-          made fullstack CMS. Learned many things that step up the level on how
-          to boost performance and handle to deployment. All this things to the
-          production level.
+        <Step title="First Research Paper Published 🔬" succeed>
+          <ul>
+            <li>
+              Published a machine learning research paper in{" "}
+              <a
+                href="https://ieeexplore.ieee.org/document/9624252"
+                rel="noreferrer"
+                target="_blank"
+              >
+                IEEE
+              </a>
+              , A Medical Pre-Diagnosis System for Histopathological Image of
+              Breast Cancer. For more information of the paper, have a look at
+              it on{" "}
+              <a
+                href="https://arxiv.org/abs/2109.07878"
+                rel="noreferrer"
+                target="_blank"
+              >
+                arXiv
+              </a>
+              .
+            </li>
+          </ul>
         </Step>
-        <Step title="HeckarNews - Hacker News Clone" succeed>
-          A fullstack project just like this blog itself but with even more
-          features implemented as a real website out there is built: handle
-          reset password by email, search functionality within site, and many
-          more. <a href="https://forum.krehwell.com/">HeckarNews</a>
+        <Step title="First Internship at Tesla 🔋 🚗" succeed>
+          <ul>
+            <li>
+              Done the first internship in my life at{" "}
+              <a href="https://www.tesla.com/" rel="noreferrer" target="_blank">
+                Tesla
+              </a>{" "}
+              (Shanghai Gigafactory).
+            </li>
+          </ul>
         </Step>
-        <Step title="10FastFingers - Average 71 WPM 💫" succeed />
-        <Step title="Learn Next.js" succeed />
-        <Step title="Learn Nest.js" succeed />
-        <Step title="Contribute to Open-source שׂ" succeed>
-          Small salvation but worth to start. Started contributing on Vim
-          plugin. I'm happy that most of thing I contributed is the things that
-          I found it disturbing.
+        <Step title="Joined University of Toronto Hyperloop Team 🚄" succeed />
+        <Step title="Joined Thiele Lab as Research Assistant 🐟" succeed>
+          <ul>
+            <li>
+              <a href="https://thielelab.ca/" rel="noreferrer" target="_blank">
+                Thiele Lab
+              </a>{" "}
+              @UofT
+            </li>
+            <li>
+              <a
+                href="https://github.com/ymart1n/BonZeb_CSharp"
+                rel="noreferrer"
+                target="_blank"
+              >
+                BonZeb
+              </a>{" "}
+              is a Bonsai library for high-resolution zebrafish behavioural
+              tracking and analysis. The project is to add the real-time
+              tracking feature to the current BonZeb library so that researchers
+              can collect accurate behavioural data easier.
+            </li>
+            <li>
+              {" "}
+              Computer Science Project - Fall 2021/Winter 2022: Machine Learning
+              in real-time analysis of multi-animal tracking experiments
+            </li>
+          </ul>
         </Step>
-        <Step title="Boomermath Made 💣" succeed>
-          Made with some of the dudes. So much satisfaction doing the project.
-          Probably a bad game for many people but the feeling on how to finish
-          it is so so super breathtaking moment.{" "}
-          <a href="https://gitlab.com/krehwell/boomermath">Boomermath</a> 💞 💖
-          💕
+        <Step title="flowboat Made 🛥️" succeed>
+          <ul>
+            <li>
+              Built flowboat, A Node-Based Machine Learning App, for Hack the
+              Valley V hackathon.
+            </li>
+            <li>
+              <a
+                href="https://devpost.com/software/flowboat-a-node-based-machine-learning-app"
+                rel="noreferrer"
+                target="_blank"
+              >
+                flowboat
+              </a>{" "}
+              is a web application that allows anyone to experience Machine
+              Learning algorithms without having to code! Using flowboat, users
+              can drag-and-drop input data, functions, and visualization outputs
+              to see what ML is really like beyond lines of code.
+            </li>
+          </ul>
         </Step>
       </ul>
       <Divider />
       <Year>2020</Year>
       <ul>
-        <Step title="Interned at Cool Code as Game Developer 🎮" succeed>
-          Cool right? yes <a href="https://coolcode.my/">Cool Code</a>. So glad
-          that this company cater all interns so well. Having the experience in
-          real working environment is so essential especially with contributing
-          to the company in making their main project as well.
+        <Step title="Learn React.js" succeed />
+
+        <Step title="First Full-Stack Web Application" succeed>
+          <ul>
+            <li>
+              Built my first{" "}
+              <a
+                href="https://github.com/ymart1n/project-luke-warm-beans"
+                rel="noreferrer"
+                target="_blank"
+              >
+                full-stack web application
+              </a>{" "}
+              with 6 amazing teammates.
+            </li>
+          </ul>
         </Step>
+        <Step title="First time applying for internship" succeed />
         <Step
-          title="Finished freeCodeCamp - Front End Development Libraries "
+          title="Entering the algorithm design &amp; analysis world 🤖"
           succeed
         />
-        <Step title="Finished freeCodeCamp - Data Visualization 📈" succeed />
-        <Step
-          title="Finished freeCodeCamp - APIs and Microservices "
-          succeed
-        />
-        <Step title="10FastFingers - Average 65 WPM 💫" succeed />
-        <Step title="First WebRTC" succeed>
-          <a href="https://callntol.vercel.app/">CallnTol</a> exists. One of
-          first web project that enables Call, Send Message, & Video Call.
-        </Step>
-        <Step title="Learn Typescript ﯤ" succeed />
-        <Step title="Re-Learn MySQL " succeed />
-        <Step title="Complete Algorithms Solving on Advent of Code" succeed />
       </ul>
-      <Divider />
-      <Year>2019</Year>
-      <ul>
-        <Step title="Get Into Web" succeed>
-          <a href="https://freecodecamp.org/">freeCodeCamp</a> helps much in
-          stepping up to the next level. College Web programming course doesn't
-          seem relevant anymore with current modern Web. Yes, I know they will
-          say that the most important is the technique. But still saying that
-          can also mean it is just same with teaching adobe flash to student.
-        </Step>
-        <Step title="Finished freeCodeCamp - Responsive Web Design" succeed />
-        <Step
-          title="Finished freeCodeCamp - JavaScript(ES6) Algorithms and Data Structures"
-          succeed
-        />
-        <Step title="Get Into Game Development " succeed />
-        <Step title="Multithread with Java " succeed>
-          Sounds trivial. But I really am learning a lot by making a multithread
-          cafe-simulation program. It teaches so many things on how to handle
-          one basic problem but complicated in solving. How to handle the
-          program flow, the responsibility of each object, connection between
-          the relational object from different thread, deadlock, and many more.
-          The most important is the convertability of this skills to be
-          transfered to other field such making game or even requesting data
-          from API in Web.
-        </Step>
-        <Step title="Vim+tmux as Main Editor 💎" succeed>
-          When it comes to development workflow, nothing beats Vim() so far.
-          Until the day I found tmux, both being my go to choices in making any
-          project, even for Unity development. Combined with HHKB, I feel
-          blessed to be alive ❤️💖.
-        </Step>
-        <Step title="10FastFingers - Average 50 WPM 💫" succeed />
-      </ul> */}
       {isShowingFullTimeline ? (
         <FullTimeline />
       ) : (
         <div style={{ textAlign: "center", marginTop: "3rem" }}>
-          {" "}
           <button
             className="button-14"
             type="button"
